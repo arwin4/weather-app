46d4d524dc5cd84157a851ba1984c13d051f8029
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-05-16 17:33:11 +0200

    Render some weather info: current icon, temp

--- src/index.js
+++ src/index.js
@@ -2,9 +2,44 @@ import './style.css';
 
 import getWeather from './weather';
 
-async function printData() {
-  const weather = await getWeather();
+let weather;
+
+async function saveWeather(location) {
+  weather = await getWeather(location);
   console.log(weather);
 }
 
-printData();
+function renderTemp(currentDiv, unit = 'C') {
+  const tempDisplay = currentDiv.querySelector('.temp');
+  if (unit === 'C') {
+    tempDisplay.textContent = weather.currentWeather.tempC;
+  } else {
+    // Fahrenheit
+    tempDisplay.textContent = weather.currentWeather.tempF;
+  }
+}
+
+async function renderWeather(location) {
+  await saveWeather(location);
+  // Current weather
+  const currentDayDiv = document.querySelector('.current');
+  const iconDisplay = currentDayDiv.querySelector('.current-weather-icon');
+  // TODO: Bigger image? (124x124)
+  iconDisplay.src = `https:${weather.currentWeather.icon}`;
+  renderTemp(currentDayDiv);
+}
+
+function newLocation() {
+  // Repopulate page with custom location information
+  const searchForm = document.querySelector('.search-form');
+  const locationInput = document.querySelector('.search-bar');
+  searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    if (locationInput.value.length < 1) return;
+    renderWeather(locationInput.value);
+    locationInput.value = '';
+  });
+}
+
+renderWeather('Longyearbyen');
+newLocation();
